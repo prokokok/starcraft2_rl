ade3b1cc3537f990a4581da132f40b2766518962
prokokok <philhoon1991@gmail.com>
2020-09-22 10:36:26 +0900

    editing scripts and uploading saved replays

--- sc2/agent/DRLAgentWithVanillaDQN_phil.py
+++ sc2/agent/DRLAgentWithVanillaDQN_phil.py
@@ -527,7 +527,7 @@ def main(unused_argv):
                map_name="Simple64",
                players=[sc2_env.Agent(sc2_env.Race.protoss),
                         sc2_env.Bot(sc2_env.Race.terran,
-                                    sc2_env.Difficulty.easy)],
+                                    sc2_env.Difficulty.medium)],
                agent_interface_format=features.AgentInterfaceFormat(
                    action_space=actions.ActionSpace.RAW,
                    use_raw_units=True,
